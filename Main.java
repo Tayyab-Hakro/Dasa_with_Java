a4ffcf18052b2327952423024f40fb4b6fa9bf7a
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-23 09:33:49 -0800

    code is solved

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-      int[] marks =  new int[8];
-      marks[0] = 1;
-      marks[1] = 2;
-      marks[2] = 3;
-      marks[3] = 4;
-      marks[4] = 5;
-      marks[5] = 6;
-      marks[6] = 7;
-for(int i = 0 ; i<marks.length ; i++){
-    System.out.println(marks[i]);
-}
 
-
-      System.out.println(marks.length);
-
-
-
+    public static void Print(){
+        System.out.println("Hi this is me");
     }
+    public static void main(String[] args) {
+        Print();
+        }
+
+
 }
