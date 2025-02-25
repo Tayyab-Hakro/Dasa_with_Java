82f0ccf9693e5e13737e18425c8c6f1a4b65e3fb
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-25 09:42:33 -0800

    Question is solved

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Main {
 
-    
     public static void main(String[] args) {
-for(int i = 0 ; i<4 ; i++){
-    for(int j= 0 ; j<5 ; j++){
-        System.out.print( "x");
+ Scanner sc = new Scanner(System.in);
+ int size = sc.nextInt();       
+ int arr[] = {1,2,3,4,5,6} ;
+ for(int i =  0 ; i<arr.length ; i++){
+    if(arr[i] ==size){
+        System.out.println("index" +i);
     }
-    System.out.println("");
-
 }
-
     }
-
-
 }
