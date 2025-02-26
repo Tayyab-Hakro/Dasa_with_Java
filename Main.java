80c2c01fae174744da1681e0344675f1905f4040
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-26 09:59:45 -0800

    Question solved

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
-
 public class Main {
+    // Take a matix Input from a User,Search for x  indices oocur in matrix
 
     public static void main(String[] args) {
- Scanner sc = new Scanner(System.in);
- int size = sc.nextInt();       
- int arr[] = {1,2,3,4,5,6} ;
- for(int i =  0 ; i<arr.length ; i++){
-    if(arr[i] ==size){
-        System.out.println("index" +i);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input rouws and colums");
+        int rows =  sc.nextInt();
+
+        int colums =  sc.nextInt();
+
+        int[][] arr = new int [rows][colums];
+    
+        for(int i = 0 ; i<rows ; i++){
+            for(int j = 0 ; j<colums ; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i= 0 ; i<rows ; i++){
+            for(int j = 0 ; j<colums ; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
-}
-    }
+
 }
