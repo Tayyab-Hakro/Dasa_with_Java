d88ae0731b42cc188ca6e11f5a950ade03309de7
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-26 13:06:09 -0800

    Question is solved

--- Main.java
+++ Main.java
@@ -1,29 +1,14 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 public class Main {
-    // Take a matix Input from a User,Search for x  indices oocur in matrix
-
+//Reverse the String in java
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("input rouws and colums");
-        int rows =  sc.nextInt();
-
-        int colums =  sc.nextInt();
-
-        int[][] arr = new int [rows][colums];
+       String name = "hellow" ;
+        for(int i = name.length()-1; i>=0 ; i--){
+            System.out.println(name.charAt(i));
+        }
+        System.out.println(name);
     
-        for(int i = 0 ; i<rows ; i++){
-            for(int j = 0 ; j<colums ; j++){
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        for(int i= 0 ; i<rows ; i++){
-            for(int j = 0 ; j<colums ; j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-
     }
+    
 
 }
