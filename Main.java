1d0d95a0473f0fab98b1586dede42c6e73b3b16b
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-27 10:55:51 -0800

    Question

--- Main.java
+++ Main.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 public class Main {
 //Reverse the String in java
     public static void main(String[] args) {
-       String name = "hellow" ;
-        for(int i = name.length()-1; i>=0 ; i--){
-            System.out.print(name.charAt(i));
-        }
-    
+       
     }
+}
     
 
-}
