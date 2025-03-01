4a33995a0384440795d85e8df82c24949f10fa10
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-01 10:44:31 -0800

    Recursion in dsa java

--- Dsa.java
+++ Dsa.java
@@ -1,23 +1,15 @@
 public class Dsa {
-//Bubble Sort 
-   public static void PrintArr( int val[]){
-    for(int i = 0; i<val.length ;i++){
-        System.out.println(val[i]);
-    }
-} 
+ public static void Printa(int  n) {
+    if(n == 0){return ; }
+ System.out.println(n);
+    Printa(n - 1);
+    
+ }
 public static void main(String[] args) {
- int arr[] = {7,8,3,4,1};
- for(int i = 0 ; i<arr.length-1;i++){
-    for(int j = 0 ;j<arr.length-i-1 ;j++){
-        if(arr[j] > arr[j+1]){
-          
-            int tem = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = tem;
-        }
-    }
- } 
- PrintArr(arr);
+
+    Printa(5);
 }
 
+
 }
+
