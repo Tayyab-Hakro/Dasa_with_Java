487c75a89da44829f0db8fc3c58cb68d57dc2ca1
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-02 07:09:46 -0800

    Recursion day 2

--- Dsa.java
+++ Dsa.java
@@ -1,11 +1,11 @@
 public class Dsa {
- public static void Printa(int  n) {
-    if(n == 0){return ; }
- System.out.println(n);
-    Printa(n - 1);
-    
- }
+ public static void Printa(int  n ) {
+    int multiply  = n;
+    if( n ==0 ){return  ;};
+    Printa(  multiply * (n-1));
+}
 public static void main(String[] args) {
+    System.out.println("This is string");
 
     Printa(5);
 }
