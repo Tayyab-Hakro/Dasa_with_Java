bf9f6992b8c87f332ece1234cd8a00a39382788e
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-08 09:32:13 -0800

    Linked Listed

--- Dsa.java
+++ Dsa.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Dsa {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number to print its multiplication table: ");
-        int number = scanner.nextInt();  // Get input from user
-        
-        // Loop from 1 to 10 for the multiplication table
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " x " + i + " = " + (number * i));
-        }
+        LinkedList <String> Str = new LinkedList<String>();
 
-        scanner.close();  // Close scanner to avoid resource leak
+        Str.addFirst("This");
+        Str.addFirst("is");
+        Str.addFirst("a");
+        Str.addFirst("Book");
+        Str.removeLast();
+
+        System.out.println(Str);
     }
 }
