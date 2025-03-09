94a9b9851f318c9e678acbb53df873f09da59f6c
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-09 09:17:13 -0700

    Question is solved

--- Dsa.java
+++ Dsa.java
@@ -1,14 +1,13 @@
-import java.util.*;
 public class Dsa {
     public static void main(String[] args) {
-        LinkedList <String> Str = new LinkedList<String>();
+        int a = 5;
+        int b = 6;
 
-        Str.addFirst("This");
-        Str.addFirst("is");
-        Str.addFirst("a");
-        Str.addFirst("Book");
-        Str.removeLast();
+        a = a + b; // a becomes 11
+        b = a - b; // b becomes 5
+        a = a - b; // a becomes 6
 
-        System.out.println(Str);
+        System.out.println("a: " + a); // 6
+        System.out.println("b: " + b); // 5
     }
 }
