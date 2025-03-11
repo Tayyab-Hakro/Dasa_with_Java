63e3ae676070dda3cd1dd3540d95215445747a23
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-11 09:19:01 -0700

    Learning Syntax of java

--- Dsa.java
+++ Dsa.java
@@ -1,13 +1,18 @@
 public class Dsa {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
 
-        a = a+b;
-        b= a-b;
-        a= a-b;
-
-        System.out.println("a: " + a); // 6
-        System.out.println("b: " + b); // 5
+public static Number LargestEle(int arr[]) {
+    // Find the Largest Element in an Array
+int max = arr[0];
+    for(int i = 0 ; i <arr.length ;i++){
+        if (  arr[i]   >  max){
+            max = arr[i];
+        }
     }
+return max;
+}
+public static void main(String[] args) {
+    int arr[] = {1,2,3,10,23,9,4};
+    
+    System.out.println(LargestEle(arr));
+}
 }
