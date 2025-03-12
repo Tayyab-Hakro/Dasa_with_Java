e1030e1f868010ae62e62c96a09641463c606c0f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-12 11:10:53 -0700

    Praticeing Recursion

--- Dsa.java
+++ Dsa.java
@@ -2,15 +2,9 @@ public class Dsa {
 
 public static Number LargestEle(int arr[]) {
     // Find the Largest Element in an Array
-int max = arr[0];
-    for(int i = 0 ; i <arr.length ;i++){
-        if (  arr[i]   >  max){
-            max = arr[i];
-        }
-    }
-return max;
+
 }
-public static void main(String[] args) {
+    public static void main(String[] args) {
     int arr[] = {1,2,3,10,23,9,4};
     
     System.out.println(LargestEle(arr));
